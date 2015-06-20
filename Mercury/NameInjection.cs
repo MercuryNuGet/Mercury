e5e72a95cb7c65b59bdd5ceb63712eb81ff3c476
Alan Evans <thealanevans@googlemail.com>
2015-06-20 18:21:39 +0100

    Optimization and constant

--- Mercury/NameInjection.cs
+++ Mercury/NameInjection.cs
@@ -5,12 +5,15 @@ namespace Mercury
 {
     internal static class NameInjection
     {
+        private static readonly string Prefix = "#";
+
         internal static string Inject(string str, object d)
         {
+            if (!str.Contains(Prefix)) return str;
             Type t = d.GetType();
             foreach (var p in t.GetProperties().OrderByDescending(p => p.Name))
             {
-                str = str.Replace("#" + p.Name, p.GetValue(d, null).ToString());
+                str = str.Replace(Prefix + p.Name, p.GetValue(d, null).ToString());
             }
             return str;
         }
