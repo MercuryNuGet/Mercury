70046f66047d0bd4700f2af9c7429b6c3c529311
Alan Evans <thealanevans@googlemail.com>
2015-06-21 18:01:49 +0100

    Test proving each with,assert pair is run

--- MercuryTests/WithTests.cs
+++ MercuryTests/WithTests.cs
@@ -67,5 +67,44 @@ namespace MercuryTests
 
             Assert.AreEqual(6, tests.Count());
         }
+
+        [Test]
+        public void Each_data_assert_pair_is_executed()
+        {
+            int[] array = new int[6];
+            ISpecification spec = "With example"
+                .Arrange()
+                .With(new { a = 1 })
+                .With(new { a = 2 })
+                .With(new { a = 3 })
+                .Assert((sut, d) => array[d.a - 1]++)
+                .Assert((sut, d) => array[d.a + 2]++);
+
+            foreach (var test in spec.EmitAllRunnableTests())
+                test.Run();
+
+            Assert.AreEqual(6, array.Sum());
+        }
+
+        [Test]
+        public void Each_data_assert_pair_and_their_act_is_executed()
+        {
+            int[] array = new int[12];
+            ISpecification spec = "With example"
+                .Arrange()
+                .With(new { a = -1 })
+                .With(new { a = -2 })
+                .With(new { a = -3 })
+                .With(new { a = -4 })
+                .Act((sut, d) => -d.a)
+                .Assert((s, d) => array[s - 1]++)
+                .Assert((s, d) => array[s + 2]++)
+                .Assert((s, d) => array[s + 5]++);
+
+            foreach (var test in spec.EmitAllRunnableTests())
+                test.Run();
+
+            Assert.AreEqual(12, array.Sum());
+        }
     }
 }
