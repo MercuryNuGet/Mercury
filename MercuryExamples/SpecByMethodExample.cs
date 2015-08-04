0511cb056ff198cc5f15871d9f5b29cdc4844149
Alan Evans <alan.evans@burrows.yr.com>
2015-08-04 09:48:07 +0100

    Spec by method example

---
+++ MercuryExamples/SpecByMethodExample.cs
@@ -0,0 +1,24 @@
+﻿using Mercury;
+using NUnit.Framework;
+
+namespace MercuryExamples
+{
+    internal class SpecByMethodExample : SpecificationByMethod
+    {
+        protected override void Cases()
+        {
+            Spec("Example of spec defined in method".Assert(() => Assert.AreEqual(2, 1 + 1)));
+
+            Spec("Lets you space out tests".Assert(() => Assert.AreEqual(2, 1 + 1)));
+
+            for (int i = 0; i < 10; i++)
+            {
+                Spec("And even lets you create specs dynamically #i"
+                    .Arrange()
+                    .With(new {i})
+                    .Act(data => data.i*10)
+                    .Assert((result, data) => Assert.IsTrue(result%10 == 0)));
+            }
+        }
+    }
+}
